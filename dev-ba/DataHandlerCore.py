f932ebea8267cf88a66e17b2d786e9bbddb69f01
duerkop <t.duerkop@tu-braunschweig.de>
2024-09-11 11:47:06 +0200

    add[*DataHandlerCore*] added functions to be implemented

--- dev-ba/DataHandlerCore.py
+++ dev-ba/DataHandlerCore.py
@@ -6,3 +6,16 @@ class DataHandler(threading.Thread):
         super(DataHandler, self).__init__()
         self.receiveQueue = receiveQueue
         self.sendQueue = sendQueue
+
+    def sendThread(self):
+        # TODO sort packets to data or cmd queue
+        pass
+
+    def receiveThread(self):
+        # TODO check for packets in data or cmd queue
+        # TODO further distribute packets to extensions and/or DB
+        pass
+
+    def toSendQueue(self, ptype, destination, payload):
+        # Function to call from i.e. extensions to send messages
+        pass
