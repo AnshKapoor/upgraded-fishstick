dcadc3d02286981de1f5aacada9475dd71c4d901
Chen Jiang <chen.jiang@tu-braunschweig.de>
2025-09-03 15:56:32 +0200

    Solve bugs for settings button

--- gspy-package/src/gspy_egse/gui/screens/simulator.py
+++ gspy-package/src/gspy_egse/gui/screens/simulator.py
@@ -1,4 +1,4 @@
-from gspy_egse.gui.plugins.juice.BrickMk4_Plugin_service1 import BrickMk4Widget as Widget
+from gspy_egse.gui.plugins.juice.BrickMk4_Plugin_service1 import BrickMk4Service1Widget as Widget
 
 SCREEN_NAME = ("Simulator")
 VERSION = 1
